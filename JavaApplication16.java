6ce9fdd05a2fd26672bf2ee258b2b7acb83ee41c
lakshmisk803 <lakshmisk803@gmail.com>
2025-04-15 14:22:17 +0530

    Update JavaApplication16.java
    change by lakshmi

--- JavaApplication16.java
+++ JavaApplication16.java
@@ -15,6 +15,7 @@ public class JavaApplication16 {
      */
     public static void main(String[] args) {
         System.out.println("hello laks and teams");
+        System.out.println("😂😂😂");
         // TODO code application logic here
     }
     
